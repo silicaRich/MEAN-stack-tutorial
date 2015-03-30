6dceea5f26d14286b00e725264bd8563a64e253d
silicarich <nicole.flokos@gmail.com>
2015-03-29 23:26:01 -0400

    simple node application
    -Bower is like a package installer
    -Nodemon runs your server automatically
    -Jade is simple HTML
    -to make a leading '.' file, use '.xyz.' instead of '.xyz'.

---
+++ server.js
@@ -0,0 +1,20 @@
+﻿var express = require('express');
+
+//dev or production mode?
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+//express application
+var app = express();
+
+// set views property
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+//what happens @ root of application, will route
+app.get('*', function (req, res) {
+    res.render('index');
+});
+
+var port = 3030;
+app.listen(port);
+console.log('server running on port ', port);
