6dfad774ad9bba571e638c581710bb60305e5e90
silicarich <nicole.flokos@gmail.com>
2015-03-30 22:17:23 -0400

    added some modules
    added & configed stylus, which serves up css files
    added morgan, an express logger
    set up static routing to public directory, making references to /public
    ez

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-﻿var express = require('express');
+﻿var express = require('express'),
+stylus = require('stylus'), // used to serve up css files
+logger = require('morgan'),
+bodyParser = require('body-parser');
 
 //dev or production mode?
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -6,9 +9,31 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 //express application
 var app = express();
 
+//stylus middleware init
+function compile(str, path) {
+    return stylus(str).set('filename', path); 
+}
+
+
 // set views property
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+
+//turn on express's logging
+app.use(logger('dev'));
+
+//stylus middleware configuration
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public', //current directory + public
+        compile: compile
+    }
+));
+
+//set up static routing to public directory
+//serve file when request maps to public directory
+app.use(express.static(__dirname + '/public'));
+
 
 //what happens @ root of application, will route
 app.get('*', function (req, res) {
